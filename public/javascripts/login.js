9c81627d8157830bda72a3a5562c484578190431
Lars Voigt <lars.voigt@dzb.de>
2015-09-14 15:13:17 +0200

    - initial commit

---
+++ public/javascripts/login.js
@@ -0,0 +1,13 @@
+(function () {
+
+
+    $("input").keydown(function (e) {
+            hideLoginFailedToolTip();
+    });
+
+    function hideLoginFailedToolTip() {
+        $("#loginFailed").attr("aria-hidden", "true");
+        $("#loginFailed").addClass("hidden");
+    }
+
+})();
---
+++ routes/index.js
@@ -0,0 +1,99 @@
+var express = require('express');
+var passport = require('passport');
+var Account = require('../models/account');
+var router = express.Router();
+var request = require('request');
+var eReaderURL = 'http://dzbvm-badi.dzbnet.local:8081';
+
+
+/***************************************************************************
+ *  eReader                                                                *
+ ***************************************************************************/
+
+router.get('/', ensureAuthenticated, eReaderRoute);
+router.get('/css/*', ensureAuthenticated, eReaderRoute);
+router.get('/scripts/*', ensureAuthenticated, eReaderRoute);
+router.get('/images/*', ensureAuthenticated, eReaderRoute);
+router.get('/fonts/*', ensureAuthenticated, eReaderRoute);
+router.get('/epub_content/*', ensureAuthenticated, eReaderRoute);
+
+// full text search 
+router.get('/matcher', ensureAuthenticated, eReaderRoute);
+router.get('/search', ensureAuthenticated, eReaderRoute);
+
+function eReaderRoute(req, res) {
+
+    var url = eReaderURL + req.url;
+    //console.log(url);
+    req.pipe(request(url)).pipe(res);
+}
+
+function ensureAuthenticated(req, res, next) {
+
+    //console.log("isAuthenticated  " + req.isAuthenticated());
+    if (req.isAuthenticated())
+        return next();
+    
+    res.redirect('/login');
+}
+
+/***************************************************************************
+ *  register                                                               *
+ ***************************************************************************/
+router.get('/register', function (req, res) {
+    res.render('register', {});
+});
+
+router.post('/register', function (req, res, next) {
+    Account.register(new Account({username: req.body.username}), req.body.password, function (err, account) {
+        if (err) {
+            return res.render("register", {info: "Sorry. That username already exists. Try again."});
+        }
+
+        passport.authenticate('local')(req, res, function () {
+            req.session.save(function (err) {
+                if (err) {
+                    return next(err);
+                }
+                res.redirect('/');
+            });
+        });
+    });
+});
+
+/***************************************************************************
+ *  login                                                                  *
+ ***************************************************************************/
+router.get('/login', function (req, res) {
+    res.render('login', {user: req.user, message: req.flash('error')});
+});
+
+
+router.post('/login', passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/login',
+        failureFlash: true
+    })
+);
+
+/***************************************************************************
+ *  logout                                                                 *
+ ***************************************************************************/
+router.get('/logout', function (req, res, next) {
+    req.logout();
+    req.session.save(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/');
+    });
+});
+
+/***************************************************************************
+ *  test                                                                   *
+ ***************************************************************************/
+router.get('/test', function (req, res) {
+    res.status(200).send("Indeed!");
+});
+
+module.exports = router;
